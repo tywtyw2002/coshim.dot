a17ccf0b7c09758b7dcb4b2b9dc194301a0bde66
Tianyi Wu <tywtyw2002@gmail.com>
2016-10-29 02:16:24 -0600

    Themes: VI-mode feature.

--- zsh_custom/editor.zsh
+++ zsh_custom/editor.zsh
@@ -7,4 +7,17 @@ command -v vim >/dev/null 2>&1 && {
 
 export EDITOR=$edit
 
+# Allow command line editing in an external editor.
+autoload -Uz edit-command-line
+bindkey -M vicmd 'v' edit-command-line
 
+zle -N edit-command-line
+
+
+# Updates editor information when the keymap changes.
+function zle-keymap-select() {
+    zle reset-prompt
+    zle -R
+}
+
+zle -N zle-keymap-select
