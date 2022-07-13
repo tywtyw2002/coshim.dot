d63f755a91a7f2267d6f6735ca2efc50dde7ca14
Landon Wu <tywtyw2002@gmail.com>
2022-07-12 18:14:14 -0600

    Add transcrypt

--- installer/script/transcrypt.sh
+++ installer/script/transcrypt.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env zsh
 
-INSTALL_PATH=$HOME/.my_config/mybin/local
-BASE_URL="https://bypass.c70.dev/"
-LINUX_KEY="cdot.starship.linux"
-MACOS_KEY="cdot.starship.macos"
-OUTPUT_BIN="starship"
-
+SCRIPT_URL="https://raw.githubusercontent.com/elasticdog/transcrypt/main/transcrypt"
+LOCALBIN=$HOME/.my_config/mybin/local
+OUTPUT_BIN="transcrypt"
 
 confirm() {
   echo -n "You already have $OUTPUT_BIN installed. Update? [y/N]"
@@ -22,23 +19,17 @@ echo -e "#        \033[0;35mInstalling $OUTPUT_BIN\033[0;34m        #"
 echo -e "#################################\033[0m"
 
 
-if [[ -f $INSTALL_PATH/$OUTPUT_BIN ]]; then
+if [[ -f $LOCALBIN/$OUTPUT_BIN ]]; then
     confirm && exit 0
-fi
-
-DOWNLOAD_URL=$BASE_URL$LINUX_KEY
-
-if [[ $(uname) = "Darwin" ]]; then
-    DOWNLOAD_URL=$BASE_URL$MACOS_KEY
 fi
 
 command -v curl >/dev/null 2>&1 || { echo "CURL not found. Please install curl." ; exit 1 }
 
-curl -fSL $DOWNLOAD_URL > $INSTALL_PATH/$OUTPUT_BIN
-chmod +x $INSTALL_PATH/$OUTPUT_BIN
+curl -fSL $DOWNLOAD_URL > $LOCALBIN/$OUTPUT_BIN
+chmod +x $LOCALBIN/$OUTPUT_BIN
 
 # Update local bins
 source $HOME/.zgen/zgen.zsh
-zgen bin $INSTALL_PATH
+zgen bin $LOCALBIN
 
 echo "Done."
