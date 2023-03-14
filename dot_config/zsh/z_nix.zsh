be3701e4d44980d77b94e20b210a0b2e21dd5e16
Landon Wu <tywtyw2002@gmail.com>
2023-03-14 03:49:02 -0600

    fix(zsh): nix hm fpath

--- dot_config/zsh/z_nix.zsh
+++ dot_config/zsh/z_nix.zsh
@@ -5,6 +5,6 @@ for profile in ${(z)NIX_PROFILES}; do
 done
 
 if [[ ! -z ${(z)NIX_USER_PROFILE_DIR} ]] && [[ -e $NIX_USER_PROFILE_DIR/home-manager/home-path/share/zsh ]]; then
-    profile=$NIX_USER_PROFILE_DIR/home-manager/home-path/share/zsh
-    fpath+=($profile/share/zsh/site-functions $profile/share/zsh/$ZSH_VERSION/functions $profile/share/zsh/vendor-completions)
+    zshare=$NIX_USER_PROFILE_DIR/home-manager/home-path/share/zsh
+    fpath+=($zshare/site-functions $zshare/$ZSH_VERSION/functions $zshare/vendor-completions)
 fi
